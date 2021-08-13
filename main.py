a2b09ce075f80f29192ca93be35ef7a87e587c19
wusimon51 <wusimon51@gmail.com>
2021-08-13 17:22:04 -0400

    create visualization with d3

--- main.py
+++ main.py
@@ -6,22 +6,29 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, url_for
 
 
-def parse_courses(course):
-    course = re.sub(r'\..+', '', course)
-    if re.match(r'^.+\d', course) is None or re.match(r'^[nN]ot', course) is not None:
+def parse_courses(course_string, prereq_string):
+    global nodes, links
+    nodes.add(course_string)
+    prereq_string = re.sub(r'\..+', '', prereq_string)
+    if re.match(r'^.+\d', prereq_string) is None or re.match(r'^[nN]ot', prereq_string) is not None:
         return []
     else:
-        result = []
-        departments = re.finditer(r'(?P<dept>([A-Z][&/\s]?){2,}(?=[\s\d]))', course)
-        nums = re.finditer(r'(?<!\d)\d{3}(?!\d)', course)
+        # list of department names
+        departments = re.finditer(r'(?P<dept>([A-Z][&/\s]?){2,}(?=[\s\d]))', prereq_string)
+        # list of course numbers
+        nums = re.finditer(r'(?<!\d)\d{3}(?!\d)', prereq_string)
+        # combine both lists, sort by position found in string to preserve order
         associations = sorted([dept for dept in departments] + [num for num in nums], key=lambda match: match.start())
         current_dept = None
         for association in associations:
             try:
+                # if item is a department name
                 current_dept = association.group('dept').replace(' ', '')
             except IndexError:
-                result.append(current_dept + ' ' + association.group())
-        return result
+                # if item is a course number
+                course = current_dept + ' ' + association.group()
+                nodes.add(course)
+                links.append({'source': course, 'target': course_string})
 
 
 # connect to website
@@ -32,13 +39,20 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 course_divs = soup.find_all('div', class_='courseblock')
 
+# TODO create nodes and links arrays
 # associate courses and prereqs in dictionary (course -> prereq)
-course_map = {}
+
+nodes = set()
+links = []
 for div in course_divs:
-    prereq_list = []
+    # string for course in question
     course_name = div.find('span', class_='courseblockcode').text.replace(u'\xa0', '').replace('\u200b', '')
-    prereq = div.find('span', class_='cbextra-data').text.replace(u'\xa0', ' ').replace('\u200b', '')
-    course_map[re.sub(r'(?<=[\sa-zA-Z])\d', lambda match: ' ' + match.group(), course_name)] = parse_courses(prereq)
+    # string containing course's prerequisites
+    prereqs = div.find('span', class_='cbextra-data').text.replace(u'\xa0', ' ').replace('\u200b', '')
+
+    # normalize course's name
+    course_name = re.sub(r'(?<=[\sa-zA-Z])\d', lambda match: ' ' + match.group(), course_name)
+    parse_courses(course_name, prereqs)
 
 # display courses
 app = Flask(__name__)
@@ -52,7 +66,7 @@ def display():
 @app.route('/data')
 def send_data():
     global course_map
-    return json.dumps(course_map)
+    return json.dumps({'nodes': [{'id': node} for node in nodes], 'links': links})
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,11 +1,54 @@
 fetch('/data')
     .then(function (response) {
-        response.json().then(
-            function (courseMap) {
-                for (const [key, value] of Object.entries(courseMap)) {
-                    console.log(key, value);
-                }
-            }
-        )
+        response.json().then((courseMap) => {
+            const nodes = courseMap.nodes;
+
+            const links = courseMap.links;
+            const width = d3.select('svg').attr('width');
+            const height = d3.select('svg').attr('height');
+
+            const simulation = d3.forceSimulation(nodes)
+                .force("charge", d3.forceManyBody())
+                .force("link", d3.forceLink(links).id(d => d.id))
+                .force("center", d3.forceCenter(width / 2, height / 2));
+
+            const svg = d3.select('svg')
+                .call(d3.zoom().on('zoom', (e) => {
+                    svg
+                        .selectAll('circle')
+                        .attr('transform', e.transform);
+                    svg
+                        .selectAll('line')
+                        .attr('transform', e.transform)
+
+                }));
+
+            const circles = svg
+                .selectAll('circle')
+                .data(nodes)
+                .enter()
+                .append('circle')
+                .attr('r', 8);
+
+            const lines = svg
+                .selectAll('line')
+                .data(links)
+                .enter()
+                .append('line')
+                .attr('stroke', 'black');
+
+            simulation.on('tick', () => {
+                circles
+                    .attr('cx', (node) => node.x)
+                    .attr('cy', (node) => node.y);
+                lines
+                    .attr('x1', (link) => link.source.x)
+                    .attr('y1', (link) => link.source.y)
+                    .attr('x2', (link) => link.target.x)
+                    .attr('y2', (link) => link.target.y)
+            })
+
+            // TODO add dragging
+        })
     });
 
