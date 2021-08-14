d6ec552c1b3c9bdad2b6a6af4f2453fbb4e34d6e
wusimon51 <wusimon51@gmail.com>
2021-08-14 11:00:55 -0400

    fix positioning of text to top right of node

--- static/main.js
+++ static/main.js
@@ -1,15 +1,17 @@
 fetch('/data')
     .then(function (response) {
         response.json().then((courseMap) => {
-
-            const nodes = courseMap.nodes;
-            const links = courseMap.links;
+            // TODO untangle nodes
+            const nodes = courseMap.nodes.map(node => Object.create(node));
+            const links = courseMap.links.map(link => Object.create(link));
             const width = d3.select('svg').attr('width');
             const height = d3.select('svg').attr('height');
             const simulation = d3.forceSimulation(nodes)
-                .force('charge', d3.forceManyBody())
-                .force('link', d3.forceLink(links).id(d => d.id).strength(0.01))
-                .force('center', d3.forceCenter(width / 2, height / 2));
+                .force('charge', d3.forceManyBody().strength(-200))
+                .force('link', d3.forceLink(links).id(d => d.id))
+                .force('center', d3.forceCenter(width / 2, height / 2))
+                // .force('x', d3.forceX(width / 2))
+                // .force('y', d3.forceY(height / 2))
 
             const svg = d3.select('svg')
                 .call(d3.zoom().on('zoom', (e) => {
@@ -31,7 +33,7 @@ fetch('/data')
                 .data(links)
                 .enter()
                 .append('line')
-                .attr('stroke', '#A9A9A9');
+                .attr('stroke', '#A9A9A9')
 
             const circles = svg
                 .append('g')
@@ -49,8 +51,6 @@ fetch('/data')
                 .data(nodes)
                 .enter()
                 .append('text')
-                .attr('text-anchor', 'middle')
-                .attr('alignment-baseline', 'middle')
                 .style('pointer-events', 'none')
                 .style('fill', 'blue')
                 .text((node) => node.id);
@@ -65,9 +65,8 @@ fetch('/data')
                     .attr('x2', (link) => link.target.x)
                     .attr('y2', (link) => link.target.y)
                 text
-                    .attr('x', (node) => node.x)
-                    .attr('y', (node) => node.y)
+                    .attr('x', (node) => node.x + 10)
+                    .attr('y', (node) => node.y - 10)
             })
         })
     });
-
