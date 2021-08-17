c3785dacb991d0c098b779af4090ab8b26b1f9c2
wusimon51 <wusimon51@gmail.com>
2021-08-16 23:55:23 -0400

    add ampersand to excluded chars in label IDs

--- static/main.js
+++ static/main.js
@@ -10,7 +10,7 @@ fetch('/data')
                 .force('link', d3.forceLink(links).id(d => d.id).distance(100))
                 .force('center', d3.forceCenter(width / 2, height / 2))
                 .force('x', d3.forceX())
-                .force('y', d3.forceY())
+                .force('y', d3.forceY());
 
             const svg = d3.select('svg')
                 .call(d3.zoom().on('zoom', (e) => {
@@ -23,7 +23,7 @@ fetch('/data')
                     svg
                         .selectAll('text')
                         .attr('transform', e.transform);
-                }))
+                }));
 
             const defs = svg
                 .append('defs')
@@ -40,7 +40,7 @@ fetch('/data')
                 .attr('orient', 'auto')
                 .append('path')
                 .attr('d', 'M0,-1.5L5,0L0,1.5')
-                .attr('fill', '#A9A9A9')
+                .attr('fill', '#A9A9A9');
 
             // TODO add links to hover
             const lines = svg
@@ -51,7 +51,7 @@ fetch('/data')
                 .enter()
                 .append('line')
                 .attr('stroke', '#A9A9A9')
-                .attr('marker-end', 'url(#arrow)')
+                .attr('marker-end', 'url(#arrow)');
 
             // TODO style circles
             const circles = svg
@@ -66,21 +66,19 @@ fetch('/data')
                     const node = d3.select(this);
                     svg
                         .append('g')
-                        .attr('id', d.id.replace(/[\s\/]/g, ''))
+                        .attr('id', d.id.replace(/[\s\/&]/g, ''))
                         .append('text')
-                        .attr('dx', parseFloat(node.attr('cx')) + 5)
-                        .attr('dy', parseFloat(node.attr('cy')) - 5)
+                        .attr('dx', parseFloat(node.attr('cx')) + 8)
+                        .attr('dy', parseFloat(node.attr('cy')) - 8)
                         .attr('transform', node.attr('transform'))
                         .attr('fill', 'blue')
                         .style('pointer-events', 'none')
                         .text(d.id);
-                    console.log(parseFloat(node.attr('cx')) + 500);
                 })
                 .on('mouseout', function (event, d) {
-                    d3.select('#' + d.id.replace(/[\s\/]/g, '')).remove();
-                })
+                    d3.select('#' + d.id.replace(/[\s\/&]/g, '')).remove();
+                });
 
-            // TODO fix text positioning
             simulation.on('tick', () => {
                 circles
                     .attr('cx', (node) => node.x)
@@ -89,7 +87,7 @@ fetch('/data')
                     .attr('x1', (link) => link.source.x)
                     .attr('y1', (link) => link.source.y)
                     .attr('x2', (link) => link.target.x)
-                    .attr('y2', (link) => link.target.y)
+                    .attr('y2', (link) => link.target.y);
             });
             return svg.node();
         })
